2e4a7d752891230f5d9338aae623a573e1edd654
Enrico Bolzonello <enribolzonello@gmail.com>
2024-01-21 19:35:04 +0100

    refactor

--- code/constants.py
+++ code/constants.py
@@ -1,4 +1,5 @@
 CATEGORIES = ["edu", "health", "transp", "food" ]
 VALUES = [["school", "kindergarten", "library"], ["clinic", "doctors", "hospital", "pharmacy"], True,["supermarket","convenience","greengrocer"]]
 TRIP_TIME = 15
+SPEED = 5
 PLACE_NAME = "Padova, Veneto, Italy"
